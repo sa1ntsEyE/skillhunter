e0053873ec827f5c9928fb74bf48e22220f5808f
Alex Kazarinov <stolbnyak2003@gmail.com>
2024-08-06 12:02:21 +0300

    v1.7

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -50,61 +50,34 @@ export class HomeComponent implements AfterViewInit {
     if (isPlatformBrowser(this.platformId)) {
 
       const titles = document.querySelectorAll('.info--title');
-      const titleObserverOptions = {
+      const sections = document.querySelectorAll('.fade-in-section');
+
+      const observerOptions = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.5
+        threshold: 0.1
       };
 
-      const titleObserver = new IntersectionObserver(this.handleIntersection, titleObserverOptions);
-      titles.forEach(title => titleObserver.observe(title));
-
-
-      const sections = document.querySelectorAll('.fade-in-section');
-      const sectionObserver = new IntersectionObserver(entries => {
+      const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
             entry.target.classList.add('is-visible');
-            sectionObserver.unobserve(entry.target);
+            observer.unobserve(entry.target);
           }
         });
-      }, { threshold: 0.5 });
-      sections.forEach(section => sectionObserver.observe(section));
+      }, observerOptions);
 
-      const phceEls = document.querySelectorAll(".phce") || [];
-      phceEls.forEach(phceEl => phceEl.addEventListener("pointermove", this.phceSetPositions));
+      titles.forEach(title => observer.observe(title));
+      sections.forEach(section => observer.observe(section));
     }
   }
 
-  handleIntersection = (entries: IntersectionObserverEntry[]) => {
+  private handleIntersection(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
     entries.forEach(entry => {
-      const index = Array.from(document.querySelectorAll('.info--title')).indexOf(entry.target as Element);
       if (entry.isIntersecting) {
-        this.activateTitle(index);
+        entry.target.classList.add('is-visible');
+        observer.unobserve(entry.target);
       }
     });
-  };
-
-  activateTitle(index: number) {
-    const titles = document.querySelectorAll('.info--title');
-    titles.forEach((title, i) => {
-      if (i === index) {
-        title.classList.add('active');
-      } else {
-        title.classList.remove('active');
-      }
-    });
-  }
-
-  phceSetPositions = ({ currentTarget: el, layerX: x, layerY: y }: any) => {
-    const { width: w, height: h } = el.getBoundingClientRect();
-    el.style.setProperty("--posX", this.phceMapPositions(x, [0, w]).toString());
-    el.style.setProperty("--posY", this.phceMapPositions(y, [0, h]).toString());
-  }
-
-  phceMapPositions(value: number, from: number[], to: number[] = [-1, 1], decimals: number = 2): number {
-    const newValue = (value - from[0]) * (to[1] - to[0]) / (from[1] - from[0]) + to[0];
-    const val = Math.min(Math.max(newValue, to[0]), to[1]);
-    return decimals && decimals > 0 ? Math.round(val * Math.pow(10, decimals)) / Math.pow(10, decimals) : val;
   }
 }
